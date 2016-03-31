9dd6f2a2d2d0a147966e6350064121bbd5019843
Tcikin Aleksandr <sunnyday.development@gmail.com>
2016-03-31 08:45:06 +0300

    Licence fix. (Added NOTICE with changing list.)

--- okhttp/src/main/java/okhttp3/HttpUrl.java
+++ okhttp/src/main/java/okhttp3/HttpUrl.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changed by sunnday.develpment@gmail.com.
+ * Added rawEncodedQuery method for setting query string without any encoding.
  */
 package okhttp3;
 
